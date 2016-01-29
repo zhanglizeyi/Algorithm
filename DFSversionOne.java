8fb50e3504c60d6b2cea441fddcb700b27d0dd4f
ze li <zhanglizeyi@zes-macbook-pro.attlocal.net>
2016-01-29 00:04:03 -0800

    run on undirected graph

---
+++ DFSversionOne.java
@@ -0,0 +1,60 @@
+//undirected graph run DFS
+
+import java.util.*;
+
+public class DFSversionOne{
+	private static int[][] adj;
+	private static int root;
+	private static int ndoe;
+	private static boolean[] visited;
+
+	DFSversionOne(int n){
+		ndoe 	= n;
+		adj 	= new int[n][n];
+		visited = new boolean[n];
+	}
+
+	//input of where should it start 
+	public static void dfs(int i){
+		visited[i] = true;  //set first one as true then print
+		System.out.println(i);
+		//check if adjcent vertices, if they do just call recursion function
+		for (int j=0; j<visited.length;j++ ) {
+			if(adj[i][j]>0 && visited[j] == false){
+				dfs(j);
+			}
+		}
+	}
+
+	public static void main(String args[]){
+		int[][] input = {{ 0, 1, 0, 1, 0, 0, 0, 0, 1 },
+ 						 { 1, 0, 0, 0, 0, 0, 0, 1, 0 },  // 1
+						 { 0, 0, 1, 1, 0, 1, 0, 1, 0 },  // 2
+						 { 1, 0, 1, 0, 1, 0, 0, 0, 0 },  // 3
+						 { 0, 0, 0, 1, 0, 0, 0, 0, 1 },  // 4
+						 { 0, 0, 1, 0, 0, 0, 1, 0, 0 },  // 5
+						 { 0, 0, 0, 0, 0, 1, 0, 0, 0 },  // 6
+						 { 0, 1, 1, 0, 0, 0, 0, 0, 0 },  // 7
+						 { 1, 0, 0, 0, 1, 0, 0, 0, 0 }};
+
+		//put the size into adjcent 
+		int size = input.length;
+		adj 	 = new int[size][size];
+		visited  = new boolean[size];
+
+		for(int i=0; i<size; i++){
+			for(int j=0;j<size;j++){
+				adj[i][j] = input[i][j];
+				System.out.print(adj[i][j]);
+			}
+			System.out.println(" ");
+		}
+
+		for(int i=0; i<size; i++){
+			visited[i] = false;
+		}
+
+		dfs(0);
+
+	}
+}
